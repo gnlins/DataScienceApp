abaf713a02e47d62eb673352d5837953bf4b3955
Gustavo Lins <gnlins@gmail.com>
2022-11-18 10:09:40 -0300

    feat: Finishing-StockMarket.py

--- P1/StockMarket.py
+++ P1/StockMarket.py
@@ -5,11 +5,11 @@ import pandas as pd
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Google!
+Shown are the stock **closing price** and **volume** of Google!
 
 """)
 # Define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = 'AAPL'
 
 # get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
@@ -18,6 +18,12 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='id', start='2010-5-31', end='2020-5-31')
 # Open High Low Close Volume Dividends Stock Splits
 
-
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
